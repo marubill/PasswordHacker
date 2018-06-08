3cbbad0bf127ed45a248198528760944b0a5d17b
中山弘瑛 <hiroaki@ld10-hiroaki-nakayama-no-macbook-air.local>
2018-06-08 14:00:25 +0900

    finish

--- PasswordHacker/ViewController.swift
+++ PasswordHacker/ViewController.swift
@@ -9,6 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //結果表示ラベル
+    @IBOutlet var resultLabel1: UILabel!
+    @IBOutlet var resultLabel2: UILabel!
+    @IBOutlet var resultLabel3: UILabel!
+    @IBOutlet var resultLabel4: UILabel!
+    
+    //カウント用ラベル
+    @IBOutlet var countLabel: UILabel!
+    
+    //秘密のパスワード
+    var password: Int = 1234
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,6 +32,55 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func start(){
+        for i in 0...9999{
+            //解析中の数字を表示する
+            countLabel.text = String(i)
+            
+            //処理を0.0005秒、待機する
+            RunLoop.main.run(until:Date(timeIntervalSinceNow: 0.0005))
+            
+            //passwordとiが一致したら...
+            if password == i {
+                print("正解は\(i)です！")
+                //passwordの桁ごとの数字を入れる配列
+                var digits = [Int]()
+                
+                for _ in 0...3{
+                    digits.append(password % 10) //j桁目の数字
+                    password = password / 10
+                }
+            
+                resultLabel1.text = String(digits[0])
+                resultLabel2.text = String(digits[1])
+                resultLabel3.text = String(digits[2])
+                resultLabel4.text = String(digits[3])
+                
+            }
+        }
+        
+    }
+    
+    @IBAction func reset(){
+        
+        //passwordを1234に再設定する
+        password = 1234
+        
+        //countlabelの表示を元に戻す
+        countLabel.text = "「START」ボタンを押して解析開始"
+        
+        //ラベルの表示をすべて0に戻す
+        resultLabel1.text = "0"
+        resultLabel2.text = "0"
+        resultLabel3.text = "0"
+        resultLabel4.text = "0"
+            
+        }
+        
+        
+    
+    
 
 
 }
